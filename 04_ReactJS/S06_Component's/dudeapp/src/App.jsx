b3b839921ff2e477f2192c5f5b297b8b1982f6a5
Kabir <sagithedeveloper@gmail.com>
2025-08-26 19:16:10 +0530

    Added the code

---
+++ 04_ReactJS/S06_Component's/dudeapp/src/App.jsx
@@ -0,0 +1,59 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1> App Component</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo unde enim
+        nisi ea a ullam nam nostrum vero amet ad distinctio alias, esse nobis
+        perspiciatis itaque minus facilis. Incidunt repudiandae, eos laudantium
+        temporibus quod minus, possimus omnis magni repellendus consequuntur
+        recusandae. Sint amet quis et exercitationem ratione nostrum, natus
+        error asperiores beatae, neque nam dignissimos accusamus minima quod
+        commodi, totam sed optio! In tempore accusantium distinctio enim
+        sapiente explicabo dolorem nihil ducimus mollitia placeat? Doloribus
+        quidem consequuntur illo voluptate odio sunt, fuga tempore saepe officia
+        error ut omnis atque impedit? Facilis in quod a laboriosam quisquam id
+        quibusdam asperiores cupiditate.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium,
+        incidunt modi deserunt repellat itaque sit vel perspiciatis nihil quidem
+        maxime autem recusandae at laborum iure fuga repudiandae dolore porro
+        excepturi blanditiis quae similique. Repellat commodi consequuntur hic
+        esse ducimus veritatis rerum aliquam dolore? Amet ducimus nihil maiores
+        ab laboriosam unde culpa in minima est omnis doloribus placeat
+        consequatur, ipsam non quis excepturi cumque cum assumenda. Deleniti
+        omnis quam dolorem hic blanditiis delectus accusamus distinctio optio
+        maiores dolor expedita perspiciatis culpa tempore ex officiis,
+        temporibus pariatur qui modi iste tempora ea ad. Esse, architecto quis
+        repellendus qui itaque rem cumque aliquam, ullam velit quia laborum
+        dolore. Incidunt delectus asperiores maiores tenetur saepe illo quidem a
+        aliquid labore! Voluptatibus, odit. Ipsa, quis. Incidunt mollitia
+        recusandae consequatur ab quae fugit nesciunt esse unde tempora quas
+        repudiandae quo nihil, voluptas sunt. Illo, voluptas in vero asperiores
+        deleniti facere ipsam animi, ullam reprehenderit enim a alias autem nam
+        soluta. Necessitatibus unde beatae, tempore cumque, provident
+        perferendis doloribus nesciunt nisi adipisci reiciendis sed voluptate
+        officia voluptatem dicta accusamus cupiditate exercitationem fugit iste
+        voluptatibus a doloremque ipsam esse. Dolorem, perferendis dolor,
+        sapiente deleniti fugit repellendus deserunt distinctio architecto,
+        quaerat amet ut perspiciatis! Ab, dignissimos aut qui ipsum non iure ex
+        sint, dolor obcaecati cum, enim perferendis laboriosam nulla ea vero
+        nisi. Tempora, voluptatibus ratione cupiditate ex quaerat vero
+        exercitationem architecto possimus. Officiis magnam provident sequi
+        cumque laudantium expedita, excepturi minus! Modi in aliquid ratione
+        minus eius enim autem. Amet nostrum consequatur quae, tempore corporis
+        pariatur quia iste reprehenderit repellendus asperiores eos temporibus
+        ratione soluta delectus cumque obcaecati provident est. Assumenda
+        debitis ipsum aliquam error minima neque blanditiis cumque dolores
+        delectus a. Voluptatum doloremque nam at iusto eveniet repudiandae autem
+        aspernatur expedita. Provident aliquam molestiae ullam ducimus, quo
+        harum recusandae illum sint culpa autem possimus ipsam ut placeat!
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ 04_ReactJS/S06_Component's/dudeapp/src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import "./Footer.css";
+function Footer() {
+  return (
+    <div className="footer">
+      <h2>Footer Component</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+        obcaecati, reprehenderit dignissimos provident quaerat dicta cumque
+        cupiditate soluta debitis beatae. Culpa fuga recusandae officiis ipsa
+        fugiat minima corporis sequi eos odio quaerat ipsam ducimus perspiciatis
+        nisi iusto, quidem quis ex? Ipsam veniam harum voluptate est sapiente
+        laboriosam, minus quis a.
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
