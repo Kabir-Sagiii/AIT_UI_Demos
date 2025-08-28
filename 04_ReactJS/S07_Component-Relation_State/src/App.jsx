164ca93989b4799c7291871c1428908d5dac47a4
Kabir <sagithedeveloper@gmail.com>
2025-08-28 19:05:04 +0530

    Added the code

---
+++ 04_ReactJS/S07_Component-Relation_State/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import A from "./components/relationship/A";
+import B from "./components/relationship/B";
+import Y from "./components/relationship/Y";
+
+function App() {
+  return (
+    <div>
+      <h1> App Component</h1>
+      <hr />
+      <A />
+      <B />
+      <Y />
+    </div>
+  );
+}
+
+export default App;
