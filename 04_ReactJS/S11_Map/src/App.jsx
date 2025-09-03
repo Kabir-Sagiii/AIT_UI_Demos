40a094ab869288e16b185deba040e5cfff480060
Kabir <sagithedeveloper@gmail.com>
2025-09-03 19:11:07 +0530

    Added the code

---
+++ 04_ReactJS/S11_Map/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Counter from "./components/counter/Counter";
+import List from "./components/map/List";
+import Names from "./components/map/Names";
+import Users from "./components/map/Users";
+function App() {
+  return (
+    <div>
+      {/* <List /> */}
+      {/* <Names /> */}
+      <Users />
+    </div>
+  );
+}
+
+export default App;
