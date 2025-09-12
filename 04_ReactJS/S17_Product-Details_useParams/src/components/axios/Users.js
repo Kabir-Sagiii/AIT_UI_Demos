90bede7f49b2fea421ea0b6de0633c9404082f40
Kabir <sagithedeveloper@gmail.com>
2025-09-12 19:15:52 +0530

    Added the code

---
+++ 04_ReactJS/S17_Product-Details_useParams/src/components/axios/Users.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+const url = "https://randomuser.me/api/?results=50";
+export function getUsers(setUsers) {
+  axios
+    .get(url)
+    .then(function (res) {
+      setUsers(res.data.results);
+    })
+    .catch(function (error) {
+      alert("failed to get users data");
+      console.log(error);
+    });
+}
---
+++ 04_ReactJS/S17_Product-Details_useParams/src/components/contact-us/ContactUs.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ContactUs() {
+  return (
+    <div>
+      <h1 style={{ color: "red", textAlign: "center", marginTop: "100px" }}>
+        ContactUs Component under implementation
+      </h1>
+    </div>
+  );
+}
+
+export default ContactUs;
